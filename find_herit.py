cdf5025ef3812a3c02c82b296e8b591cb4c06e22
bayleaflet <bayleejchristensen@gmail.com>
2024-10-03 13:32:34 -0600

    Nonfunctioning still

---
+++ find_herit.py
@@ -0,0 +1,12 @@
+
+def parse(input_lines):
+    for line in input_line:
+
+
+def main():
+    with open('origin.txt', 'r') as in_stream:
+        with open('output.txt', 'w') as out_stream:
+
+
+if __name__ == '__main__':
+    main()
